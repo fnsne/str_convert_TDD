adaa6ebf191f2073ec3cec5633b0af3d501e6d28
fnsne <watashiwafnsne@gmail.com>
2020-02-13 16:08:13 +0800

    passed char_duplicate_by_its_index

--- converter.go
+++ converter.go
@@ -7,6 +7,14 @@ func converter(input string) string {
 		first := input[0]
 		second := input[1]
 		return string(first) + "-" + strings.ToUpper(string(second)) + string(second)
+	} else if len(input) == 3 {
+		first := input[0]
+		second := input[1]
+		third := input[2]
+		return string(first) + "-" +
+			strings.ToUpper(string(second)) + string(second) + "-" +
+			strings.ToUpper(string(third)) + string(third) + string(third)
+
 	} else {
 		return strings.ToUpper(input)
 	}
