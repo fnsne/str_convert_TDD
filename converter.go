ebbc6b08b124088f743b0a9c407c3a35ea4cf3b1
fnsne <watashiwafnsne@gmail.com>
2020-02-13 16:00:23 +0800

    passed second_char_duplicate_twice

--- converter.go
+++ converter.go
@@ -3,5 +3,11 @@ package TDD_string_convertor
 import "strings"
 
 func converter(input string) string {
-	return strings.ToUpper(input)
+	if len(input) == 2 {
+		first := input[0]
+		second := input[1]
+		return string(first) + "-" + strings.ToUpper(string(second)) + string(second)
+	} else {
+		return strings.ToUpper(input)
+	}
 }
