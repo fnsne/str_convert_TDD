44464d05afc83375917850a333b6e6dee17e398c
fnsne <watashiwafnsne@gmail.com>
2020-02-13 16:11:25 +0800

    input string length == 2的改成用Join串接 '-'

--- converter.go
+++ converter.go
@@ -6,7 +6,10 @@ func converter(input string) string {
 	if len(input) == 2 {
 		first := input[0]
 		second := input[1]
-		return string(first) + "-" + strings.ToUpper(string(second)) + string(second)
+		firstStatement := string(first)
+		secondStatement := strings.ToUpper(string(second)) + string(second)
+		generatedStrings := []string{firstStatement, secondStatement}
+		return strings.Join(generatedStrings, "-")
 	} else if len(input) == 3 {
 		first := input[0]
 		second := input[1]
@@ -14,7 +17,6 @@ func converter(input string) string {
 		return string(first) + "-" +
 			strings.ToUpper(string(second)) + string(second) + "-" +
 			strings.ToUpper(string(third)) + string(third) + string(third)
-
 	} else {
 		return strings.ToUpper(input)
 	}
