723be93d99c85a95c43516917447911efb05eec9
fnsne <watashiwafnsne@gmail.com>
2020-02-13 16:23:39 +0800

    刪除重複的code

--- converter.go
+++ converter.go
@@ -3,20 +3,13 @@ package TDD_string_convertor
 import "strings"
 
 func converter(input string) string {
-	if len(input) == 2 {
+	if len(input) >= 2 {
 		var generatedStrings []string
 		for index, c := range input {
 			genString := duplicateByIndex(index, c)
 			generatedStrings = append(generatedStrings, genString)
 		}
 		return strings.Join(generatedStrings, "-")
-	} else if len(input) == 3 {
-		first := input[0]
-		second := input[1]
-		third := input[2]
-		return string(first) + "-" +
-			string(second) + string(second) + "-" +
-			string(third) + string(third) + string(third)
 	} else {
 		return strings.ToUpper(input)
 	}
